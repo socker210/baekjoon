f2d22ad03784633b24976af1c0a705f33b4eacdd
junn@testvalley <socker210@gmail.com>
2022-04-27 16:36:53 +0900

    11651-quick

--- silver/11651/11651-quick.cpp
+++ silver/11651/11651-quick.cpp
@@ -2,14 +2,20 @@
 
 using namespace std;
 
-void swap(int* n1, int* n2) {
-  int t = *n1;
+const int X = 0;
+const int Y = 1;
 
-  *n1 = *n2;
-  *n2 = t;
+void swap(int* n1[], int* n2[]) {
+  int tx = *n1[X];
+  int ty = *n1[Y];
+
+  *n1[X] = *n2[X];
+  *n1[Y] = *n2[Y];
+  *n2[X] = tx;
+  *n2[Y] = ty;
 }
 
-void sort(int n[], int start, int end) {
+void quickSort(int n[][2], int start, int end) {
   if (start >= end) return;
 
   int pivot = start;
@@ -17,29 +23,29 @@ void sort(int n[], int start, int end) {
   int right = end;
 
   while(left <= right) {
-    while(left <= end && n[left] < n[pivot]) left++;
-    while(right >= start && n[right] > n[pivot]) right--;
-    
-    if (left > right) swap(&n[right], &n[pivot]);
-    else swap(&n[left], &n[right]);
+    while(left <= end && (n[left][Y] == n[pivot][Y] ? n[left][X] <= n[pivot][X] : n[left][Y] <= n[pivot][Y])) left++;
+    while(right >= start && (n[right][Y] == n[pivot][Y] ? n[right][X] > n[pivot][X] : n[right][Y] > n[pivot][Y])) right--;
+
+    if (left > right) swap(n[right], n[pivot]);
+    else swap(n[left], n[right]);
   }
 
-  sort(n, start, right - 1);
-  sort(n, right + 1, end);
+  quickSort(n, start, right - 1);
+  quickSort(n, right + 1, end);
 }
 
 int main() {
   int N;
 
-  scanf("%d", &N);
+  cin >> N;
 
-  int n[N];
+  int n[N][2];
 
-  for (int i=0; i<N; i++) scanf("%d", &n[i]);
+  for (int i=0; i<N; i++) scanf("%d %d", &n[i][X], &n[i][Y]);
 
-  sort(n, 0, N - 1);
+  quickSort(n, 0, N - 1);
 
-  for (int i=0; i<N; i++) cout << n[i] << endl;
+  for (int i=0; i<N; i++) cout << n[i][X] << " " << n[i][Y] << "\n";
 
   return 0;
 }
--- silver/11651/11651.cpp
+++ silver/11651/11651.cpp
@@ -2,46 +2,6 @@
 
 using namespace std;
 
-struct Point {
-  int x;
-  int y;
-};
-
-void swap(Point* p1, Point* p2) {
-  Point t = *p1;
-
-  *p1 = *p2;
-  *p2 = t;
-}
-
 int main() {
-  int N;
-
-  scanf("%d", &N);
-
-  Point p[N];
-
-  for (int i=0; i<N; i++) {
-    Point t;
-
-    scanf("%d %d", &t.x, &t.y);
-
-    p[i] = t;
-  }
-
-  for (int i=0; i<N - 1; i++) {
-    for (int j=i + 1; j<N; j++) {
-      if (p[j].y < p[i].y) {
-        swap(&p[i], &p[j]);
-      } else if (p[j].y == p[i].y && p[j].x < p[i].x) {
-        swap(&p[i], &p[j]);
-      }
-    }
-  }
-
-  for (int i=0; i<N; i++) {
-    cout << p[i].x << " " << p[i].y << endl;
-  }
-
   return 0;
 }
