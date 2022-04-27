22f8d84e3c138217f7256250b8889c461c566b00
junn@testvalley <socker210@gmail.com>
2022-04-27 14:23:47 +0900

    11651-quick

--- silver/11651/11651-quick.cpp
+++ silver/11651/11651-quick.cpp
@@ -2,19 +2,42 @@
 
 using namespace std;
 
-void sort() {
-  // TODO: 퀵정렬 하기
+void swap(int* n1, int* n2) {
+  int t = *n1;
+
+  *n1 = *n2;
+  *n2 = t;
+}
+
+void sort(int n[], int start, int end) {
+  if (start >= end) return;
+
+  int pivot = start;
+  int left = start + 1;
+  int right = end;
+
+  while(left <= right) {
+    while(left <= end && n[left] < n[pivot]) left++;
+    while(right >= start && n[right] > n[pivot]) right--;
+    
+    if (left > right) swap(&n[right], &n[pivot]);
+    else swap(&n[left], &n[right]);
+  }
+
+  sort(n, start, right - 1);
+  sort(n, right + 1, end);
 }
 
 int main() {
   int N;
-  int n[N];
 
   scanf("%d", &N);
 
+  int n[N];
+
   for (int i=0; i<N; i++) scanf("%d", &n[i]);
 
-  sort();
+  sort(n, 0, N - 1);
 
   for (int i=0; i<N; i++) cout << n[i] << endl;
 
