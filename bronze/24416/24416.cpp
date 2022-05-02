a4452234ccb6286df8052b790957b363ead7bf32
junn@testvalley <socker210@gmail.com>
2022-05-02 10:00:11 +0900

    24416

--- bronze/24416/24416.cpp
+++ bronze/24416/24416.cpp
@@ -2,22 +2,30 @@
 
 using namespace std;
 
-// 점화식
-// n == 0, 0
-// n == 1, 1
-// F(n - 1) + F(n - 2), n > 2
-
 int recursion(int* cnt, int n) {
-  (*cnt)++;
+  if (n == 0 || n == 1 || n == 2) {
+    (*cnt)++;
 
-  if (n == 0) return 0;
-  if (n == 1) return 1;
+    return n == 0 ? 0 : 1;
+  }
 
   return recursion(cnt, n - 1) + recursion(cnt, n - 2);
 }
 
-void dp() {
+int dp(int* cnt, int n) {
+  int f[n + 1];
 
+  f[0] = 0;
+  f[1] = 1;
+  f[2] = 1;
+
+  for (int i=3; i<=n; i++) {
+    (*cnt)++;
+    
+    f[i] = f[i - 1] + f[i - 2];
+  }
+
+  return f[n];
 }
 
 int main() {
@@ -28,6 +36,7 @@ int main() {
   scanf("%d", &N);
 
   recursion(&recursive_cnt, N);
+  dp(&dp_cnt, N);
 
   printf("%d %d", recursive_cnt, dp_cnt);
 
