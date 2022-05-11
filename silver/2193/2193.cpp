3dac62461171fd4aca18781a809d897500f6a422
Junn <socker210@gmail.com>
2022-05-12 01:13:47 +0900

    2193

--- silver/2193/2193.cpp
+++ silver/2193/2193.cpp
@@ -2,29 +2,19 @@
 
 using namespace std;
 
-int pow(int base, int exp) {
-  int res = 1;
-
-  while(exp--) res *= base;
-
-  return res;
-}
-
 int main() {
   int N;
 
   scanf("%d", &N);
 
-  int digit = pow(2, N - 1);
-
-  int dp[digit + 1];
+  long dp[N + 1];
 
   dp[0] = 0;
   dp[1] = 1;
 
-  for (int i=2; i<=digit; i++) dp[i] = dp[i - 2] + dp[i - 1];
+  for (int i=2; i<=N; i++) dp[i] = dp[i - 2] + dp[i - 1];
 
-  printf("%d", dp[N]);
+  printf("%ld", dp[N]);
 
   return 0;
 }
