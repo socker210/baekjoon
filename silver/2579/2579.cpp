07b5af6d798ab34fae9327615f83d32bcf9da516
junn@testvalley <socker210@gmail.com>
2022-05-16 17:56:38 +0900

    2579 ...ing

--- silver/2579/2579.cpp
+++ silver/2579/2579.cpp
@@ -12,30 +12,18 @@ int main() {
 	for (int i=0; i<N; i++) scanf("%d", &n[i]);
 
 	int dp[N];
-	int cnt = 0;
 
 	dp[0] = n[0];
-	dp[1] = dp[0] + n[1];
-
-	cnt = dp[1] > dp[0] ? 1 : 0;
+	dp[1] = n[0] + n[1];
 
 	for (int i=2; i<N; i++) {
-		if (dp[i - 1] > dp[i - 2]) {
-			dp[i] = n[i] + dp[i - 1];
+		int a = n[i] + dp[i - 1] - n[i - 2];
+		int b = n[i] + dp[i - 2];
 
-			if (cnt == 2) {
-				dp[i] -= dp[i - 2];
-
-				cnt = 0;
-			} else {
-				cnt++;
-			}
-		} else {
-			dp[i] = n[i] + dp[i - 2];
-		}
+		dp[i] = a > b ? n[i] + n[i - 1] + (i > 2 ? dp[i - 3] : 0) : b;
 	}
 
-	printf("%d ", dp[N - 1]);
+	printf("%d", dp[N - 1]);
 
 	return 0;
 }
