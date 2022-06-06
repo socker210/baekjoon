6e75aae559189212eb9392cc5cb0e005e734bc5f
Junn <socker210@gmail.com>
2022-06-06 18:50:54 +0900

    4949 ...ing

---
+++ silver/4949/4949.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	freopen("example.txt", "r", stdin);
+
+	// TODO: 	
+
+	return 0;
+}
