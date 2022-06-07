7d06b749cb3c82a04d0904fb7ac738bcd5550c1b
junn@testvalley <socker210@gmail.com>
2022-06-07 17:00:10 +0900

    1874 ...ing

---
+++ silver/1874/1874.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+  freopen("example.txt", "r", stdin);
+
+  int N, n;
+
+  cin >> N;
+
+  int T[N], O[N * 2],top = -1;
+
+  for (int i=1; i<=N; i++) {
+    cin >> n;
+
+    // TODO: 처리하기
+    
+  }
+
+  return 0;
+}
