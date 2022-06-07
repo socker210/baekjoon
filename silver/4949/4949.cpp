b2073d9de20ee8c22abd3f0a745371040b586133
junn@testvalley <socker210@gmail.com>
2022-06-07 15:01:42 +0900

    solved 4949

--- silver/4949/4949.cpp
+++ silver/4949/4949.cpp
@@ -7,7 +7,27 @@ int main() {
 
 	freopen("example.txt", "r", stdin);
 
-	// TODO: 	
+	char str[101], bracket[101];
+	
+	while(true) {
+		cin.getline(str, 101);
+
+		if (str[0] == '.') break;
+
+		int top = -1;
+
+		for (int i=0; str[i]; i++) {
+			char c = str[i];
+
+			if (c != '(' && c != ')' && c != '[' && c != ']') continue;
+
+			bracket[++top] = c;
+
+			if ((c == ']' && top > 0 && bracket[top - 1] == '[') || (c == ')' && top > 0 && bracket[top - 1] == '(')) top -= 2;
+		}
+
+		cout << (top == -1 ? "yes" : "no") << '\n';
+	}
 
 	return 0;
 }
