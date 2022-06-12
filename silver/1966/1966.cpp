20404ad8abef7b55619543ed7d8b38c7a249c615
Junn <socker210@gmail.com>
2022-06-12 18:31:22 +0900

    1966 ...ing

--- silver/1966/1966.cpp
+++ silver/1966/1966.cpp
@@ -1,40 +1,42 @@
 #include <iostream>
-
 using namespace std;
-
-typedef struct Node {
-  int priority;
-} Node;
 
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(NULL);
-
   freopen("example.txt", "r", stdin);
-
   int N; cin >> N;
-
   while(N--) {
     int C, T;
-    
     cin >> C >> T;
-
-    Node* node_arr[C];
-
+    int arr[C];
     for (int i=0; i<C; i++) {
-      int t; cin >> t;
-
-      Node* node = new Node;
-
-      node->priority = t;
-
-      node_arr[i] = node;
+      int t;
+      cin >> t;
+      arr[i] = t;
+    }
+    for (int i=(C/2)-1; i>=0; i--) {
+      cout << i << ' ' <<  i * 2 + 1 << ' ' << i * 2 + 2 << '\n';
     }
 
-    
-    // TODO: 이거 우선순위 큐로 만들기
-    // build heapify
-    
-  }
 
+    // for (int i=0; i<C; i++) cout << arr[i] << '\n';
+  }
   return 0;
 }
+
+// 0 -> 1, 2
+// 1 -> 3, 4
+// 2 -> 5
+
+// i=0; i<6; i++
+
+// 루트 -> (i - 1) / 2
+// 왼쪽 -> i * 2 + 1
+// 오른쪽 -> i * 2 + 2
+
+// 0: 루트
+// 1: 루트 왼쪽 
+// 2: 루트 오른쪽
+// 3: 루트 왼쪽 왼쪽
+// 4: 루트 왼쪽 오른쪽
+// 5: 루트 오른쪽 왼쪽
