d05530761ac1ef130f019b2641fdbb56de3af591
junn@testvalley <socker210@gmail.com>
2022-06-20 17:39:38 +0900

    1966 ...ing

--- silver/1966/1966.cpp
+++ silver/1966/1966.cpp
@@ -13,35 +13,53 @@ int main() {
   int N;
   cin >> N;
   while(N--) {
-    int C, T, ans;
+    int C, T;
     cin >> C >> T;
-    int heap[C], s = 0;
-    for (int i=0;i <C;i ++) {
+    int queue[C], heap[C], s = 0;
+    for (int i=0; i<C; i++) {
       int n, j;
       cin >> n;
       j = s;
       heap[s] = n;
+      queue[i] = n;
       while(j != 0 && heap[j] > heap[(j - 1) / 2]) {
         swap(&heap[j], &heap[(j - 1) / 2]);
         j = (j - 1) / 2;
       }
       s++;
     }
-    int t_val = heap[T], idx = 0;
-    while(s--) {
-      int j;
-      j = 0;
-      ans = heap[s] == t_val ? idx++ : ans;
-      swap(&heap[j], &heap[s]);
-      while(true) {
-        // TODO: 이거 break조건 처리하기, 위의 ans처리하기
-        int c_idx = heap[j + 1] > heap[j + 2] ? j + 1 : j + 2;
-        if (heap[c_idx] > heap[j]) {
-          swap(&heap[c_idx], &heap[j]);
-          j = c_idx;
-        }else break;
+    int target_idx = T;
+    while(true) {
+      int front, rear, cnt;
+      front = cnt = 0;
+      rear = C - 1;
+      if (heap[0] == queue[front]) {
+        if (target_idx == 0) {
+          cout << cnt << '\n';
+          break;
+        } else {
+          int j;
+          s--;
+          j = 0;
+          swap(&heap[j], &heap[s]);
+          while(true) {
+            if (j >= s - 1) break;
+            int c_idx = j + 2 > s - 1 ? j + 1 : heap[j + 1] > heap[j + 2] ? j + 1 : j + 2;
+            if (heap[c_idx] > heap[j]) swap(&heap[c_idx], &heap[j]);
+            else break;
+            j = c_idx;
+          }
+          front++;
+        }
+        target_idx--;
+      } else {
+        // TODO: 이거 오류 찾기
+        int t = queue[front];
+        for (int i=front+1; i<rear-1; i++) queue[i-1] = queue[i];
+        queue[rear] = t;
+        cnt++;
+        target_idx = t == queue[target_idx] ? rear : target_idx - 1;
       }
     }
-    cout << ans << '\n';
   }
 }
