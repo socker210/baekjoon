e9c755a92e7ef98b16d1420d224d60aded1f4e8b
junn@testvalley <socker210@gmail.com>
2022-06-23 17:43:31 +0900

    solved 1966

--- silver/1966/1966.cpp
+++ silver/1966/1966.cpp
@@ -44,8 +44,12 @@ int main() {
           j = 0;
           swap(&heap[j], &heap[s]);
           while(true) {
-            if (j >= s - 1) break;
-            int c_idx = j + 2 > s - 1 ? j + 1 : heap[j + 1] > heap[j + 2] ? j + 1 : j + 2;
+            if (j * 2 >= s - 1) break;
+            if (j * 2 + 2 > s - 1) {
+              swap(&heap[j], &heap[j * 2 + 1]);
+              break;
+            }
+            int c_idx = heap[j * 2 + 1] > heap[j * 2 + 2] ? j * 2 + 1 : j * 2 + 2;
             if (heap[c_idx] > heap[j]) swap(&heap[c_idx], &heap[j]);
             else break;
             j = c_idx;
@@ -57,8 +61,9 @@ int main() {
         int t = queue[front];
         for (int i=front+1; i<=rear; i++) queue[i-1] = queue[i];
         queue[rear] = t;
-        target_idx = target_idx == 0 ? rear : target_idx - 1;
+        target_idx = target_idx == 0 ? rear - front : target_idx - 1;
       }
     }
   }
 }
+
