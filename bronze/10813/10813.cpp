fab923ca591f1d474eca256eface26f4e1967eb5
Junn <socker210@gmail.com>
2023-07-11 19:29:39 +0900

    solved 10813

---
+++ bronze/10813/10813.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N, M;
+
+	cin >> N >> M;
+
+	int basket[N + 1];
+
+	for (int i=1; i<=N; i++) basket[i] = i;
+
+	while(M--) {
+		int i, j, t;
+
+		cin >> i >> j;
+
+		t = basket[i];
+		basket[i] = basket[j];
+		basket[j] = t;
+	}
+
+	for (int i=1; i<=N; i++) cout << basket[i] << ' ';
+
+	return 0;
+}
