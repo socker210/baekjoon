e0fb4795c12a060cae35934ee97951b4c6514ddc
Junn <socker210@gmail.com>
2023-07-11 20:51:26 +0900

    solved 25083

---
+++ bronze/25083/25083.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |");
+	
+	return 0;
+}
